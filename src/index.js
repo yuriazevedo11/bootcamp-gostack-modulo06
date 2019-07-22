0c251a7c29b54baab6e239726bfe6ca9c350d9b3
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-07-22 20:50:01 -0300

    NEW - Configuração do React Navigation

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 
 import './config/ReactotronConfig';
 
+import Routes from './routes';
+
 const App = () => {
-  return (
-    <View>
-      <Text>Welcome to React Native!</Text>
-    </View>
-  );
+  return <Routes />;
 };
 
 export default App;
